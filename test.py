b7241aa564e648cece545422787f90a431f52edf
clever85fox <clever85fox@gmail.com>
2022-11-11 17:42:16 -0700

    first update

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-
+At this point in the late we are editing and making changes to the document that we uploaded into a folder, created in GitHub cloned and manually added to our personal computer files.
